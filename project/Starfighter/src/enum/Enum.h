5d327a9c33560959a90259f184be35f64688d1f3
Diego999 <diegoantognini@gmail.com>
2012-11-26 21:21:11 +0000

    Suppression of useless classes

---
+++ project/Starfighter/src/enum/Enum.h
@@ -0,0 +1,18 @@
+#ifndef ENUMERATION_H
+#define ENUMERATION_H
+
+enum Action
+{
+    Top,
+    Bottom,
+    Shoot,
+    Escape
+};
+
+enum shooter
+{
+    Player1,Player2,Other
+};
+
+
+#endif // ENUMERATION_H
--- project/Starfighter/src/game/Enum.h
+++
@@ -1,9 +0,0 @@
-#ifndef ENUM_H
-#define ENUM_H
-
-enum shooter
-{
-    Player1 = 1,Player2 = 2,Other = 3
-};
-
-#endif // ENUM_H
