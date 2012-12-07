12c0edb593db6777ce0e97f722559c3bd4a53a18
Diego999 <diegoantognini@gmail.com>
2012-12-07 08:46:31 +0000

    Trajectory + edge

--- project/Starfighter/src/game/AlienSpaceship.cpp
+++ project/Starfighter/src/game/AlienSpaceship.cpp
@@ -1,5 +1,53 @@
 #include "include/game/AlienSpaceship.h"
+#include <math.h>
 
-AlienSpaceship::AlienSpaceship(QPoint P1, QPoint P2, QPoint P3, int nbSpirales):Obstacle(x,y),Destroyable()
+AlienSpaceship::AlienSpaceship(QPoint P1, QPoint P2, QPoint P3, int nbSpirales)
+    :Obstacle(P1.x(),P1.y()),Destroyable()
 {
+    int x1 = P1.x();
+    int x2 = P2.x();
+    int x3 = P3.x();
+    int y1 = P1.y();
+    int y2 = P2.y();
+    int y3 = P3.y();
+
+    dX0 = (x3*x3*(y2-y1)+x2*x2*(y1-y3)-(x1*x1+(y1-y2)*(y1-y3))*(y2-y3))
+            /(2*(x3*(y2-y1)+x2*(y1-y3)+y1*(y3-y2)));
+    dY0 = (-x2*x2*x3+x1*x1*(-x2+x3)+x3*(y1*y1-y2*y2)+y1*(x2*x2-x3*x3+y2*y2-y3*y3)+x2*(x3*x3-y1*y1+y3*y3))
+            /(2*(x3*(y1-y2)+x1*(y2-y3)+x2*(-y1+y3)));
+    dModule = sqrt((x1-dX0)*(x1-dX0)+(y1-dY0)*(y1-dY0));
+
+    dTheta = acos((dX0+x1)/dModule);
+}
+
+void AlienSpaceship::advance(int _step)
+{
+    Obstacle::advance(_step);
+
+    dTheta-=kIntervalArgument;
+
+    x = dX0+dModule*cos(dArgument*M_PI/180.0);
+    y = dY0-dModule*sin(dArgument*M_PI/180.0);
+
+    setPos(x,y);
+}
+
+QRectF AlienSpaceship::boundingRect() const
+{
+    int l_width = pxmPicture->rect().width();
+    int l_height = pxmPicture->rect().height();
+
+    return QRectF(QPoint(x-l_width/2,y-l_height/2),QPoint(x+l_width/2,y+l_height/2));
+}
+
+QPainterPath AlienSpaceship::shape() const
+{
+    QPainterPath l_path;
+    l_path.addEllipse(boundingRect());
+    return l_path;
+}
+
+void AlienSpaceship::paint(QPainter* _painter, QStyleOptionGraphicsItem* _option, QWidget* _widget)
+{
+    _painter->drawPixmap(x,y,*pxmPicture);
 }
