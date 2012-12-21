b95087f3d8da1d9e62f82661aedef868b172a955
Sébastien Vaucher <seb.vaucher@gmail.com>
2012-12-21 21:18:14 +0000

    Update translations

--- project/Starfighter/starfighter_fr.ts
+++ project/Starfighter/starfighter_fr.ts
@@ -36,27 +36,45 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>DisplayEngine</name>
     <message>
-        <location filename="src/engine/DisplayEngine.cpp" line="127"/>
-        <location filename="src/engine/DisplayEngine.cpp" line="192"/>
+        <location filename="src/engine/DisplayEngine.cpp" line="109"/>
+        <location filename="src/engine/DisplayEngine.cpp" line="189"/>
         <source>Name : %1</source>
         <translation type="unfinished">Nom : %1</translation>
     </message>
     <message>
-        <location filename="src/engine/DisplayEngine.cpp" line="134"/>
-        <location filename="src/engine/DisplayEngine.cpp" line="198"/>
+        <location filename="src/engine/DisplayEngine.cpp" line="116"/>
+        <location filename="src/engine/DisplayEngine.cpp" line="195"/>
         <source>HP:	</source>
         <translation type="unfinished">PV :</translation>
     </message>
     <message>
-        <location filename="src/engine/DisplayEngine.cpp" line="143"/>
-        <location filename="src/engine/DisplayEngine.cpp" line="207"/>
+        <location filename="src/engine/DisplayEngine.cpp" line="125"/>
+        <location filename="src/engine/DisplayEngine.cpp" line="204"/>
         <source>Shield:	</source>
         <translation type="unfinished">Bouclier :</translation>
     </message>
     <message>
-        <location filename="src/engine/DisplayEngine.cpp" line="180"/>
+        <location filename="src/engine/DisplayEngine.cpp" line="176"/>
         <source>:</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/engine/DisplayEngine.cpp" line="329"/>
+        <location filename="src/engine/DisplayEngine.cpp" line="357"/>
+        <source>Standard shot</source>
+        <translation type="unfinished">Tir standard</translation>
+    </message>
+    <message>
+        <location filename="src/engine/DisplayEngine.cpp" line="334"/>
+        <location filename="src/engine/DisplayEngine.cpp" line="362"/>
+        <source>Cross Shot</source>
+        <translation type="unfinished">Tir croisé</translation>
+    </message>
+    <message>
+        <location filename="src/engine/DisplayEngine.cpp" line="339"/>
+        <location filename="src/engine/DisplayEngine.cpp" line="367"/>
+        <source>V Shot</source>
+        <translation type="unfinished">Tir en V</translation>
     </message>
     <message>
         <source>End of the game</source>
@@ -70,20 +88,28 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>GameEngine</name>
     <message>
-        <location filename="src/engine/GameEngine.cpp" line="172"/>
-        <location filename="src/engine/GameEngine.cpp" line="173"/>
+        <location filename="src/engine/GameEngine.cpp" line="183"/>
         <location filename="src/engine/GameEngine.cpp" line="188"/>
-        <location filename="src/engine/GameEngine.cpp" line="201"/>
+        <location filename="src/engine/GameEngine.cpp" line="202"/>
+        <location filename="src/engine/GameEngine.cpp" line="203"/>
+        <location filename="src/engine/GameEngine.cpp" line="218"/>
+        <location filename="src/engine/GameEngine.cpp" line="231"/>
         <source>End of the game</source>
-        <translation type="unfinished">Fin du jeu</translation>
+        <translation type="unfinished">Fin de la partie</translation>
     </message>
     <message>
-        <location filename="src/engine/GameEngine.cpp" line="189"/>
+        <location filename="src/engine/GameEngine.cpp" line="184"/>
+        <location filename="src/engine/GameEngine.cpp" line="219"/>
         <source>%1 has won !</source>
         <translation type="unfinished">%1 a gagné !</translation>
     </message>
     <message>
-        <location filename="src/engine/GameEngine.cpp" line="202"/>
+        <location filename="src/engine/GameEngine.cpp" line="189"/>
+        <source>No one has won ... Egality !</source>
+        <translation type="unfinished">Personne n&apos;a gagné... Égalité !</translation>
+    </message>
+    <message>
+        <location filename="src/engine/GameEngine.cpp" line="232"/>
         <source>Do you want to stop the current game ?</source>
         <translation type="unfinished">Voulez-vous quitter le jeu en cours ?</translation>
     </message>
@@ -284,12 +310,22 @@ p, li { white-space: pre-wrap; }
         <translation type="unfinished">Valider</translation>
     </message>
     <message>
-        <location filename="src/menu/OptionsDialog.cpp" line="71"/>
+        <location filename="src/menu/OptionsDialog.cpp" line="64"/>
+        <source>Invalid keys</source>
+        <translation type="unfinished">Touches invalides</translation>
+    </message>
+    <message>
+        <location filename="src/menu/OptionsDialog.cpp" line="64"/>
+        <source>Two or more keys are set to the same value</source>
+        <translation type="unfinished">Une même touche est utilisée plusieurs fois</translation>
+    </message>
+    <message>
+        <location filename="src/menu/OptionsDialog.cpp" line="79"/>
         <source>Saved</source>
         <translation type="unfinished">Enregistré</translation>
     </message>
     <message>
-        <location filename="src/menu/OptionsDialog.cpp" line="71"/>
+        <location filename="src/menu/OptionsDialog.cpp" line="79"/>
         <source>The settings were successfully saved</source>
         <translation type="unfinished">Les options ont été correctement enregistrées</translation>
     </message>
@@ -310,7 +346,7 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>SetKeyDialog</name>
     <message>
-        <location filename="src/menu/SetKeyDialog.cpp" line="8"/>
+        <location filename="src/menu/SetKeyDialog.cpp" line="7"/>
         <source>Please press the key now</source>
         <translation type="unfinished">Veuillez appuyer sur la touche voulue</translation>
     </message>
