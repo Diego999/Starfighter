6968c2f89d56bbc13b1c13e20a5678e60c9ab3b7
Diego999 <diegoantognini@gmail.com>
2013-01-20 16:17:50 +0000

    Remove warnings

--- project/Starfighter/src/engine/GameEngine.cpp
+++ project/Starfighter/src/engine/GameEngine.cpp
@@ -16,7 +16,6 @@
 
 #include "include/utils/Settings.h"
 #include "include/config/Define.h"
-#include "include/stable.h"
 
 GameEngine::GameEngine(GameMode gameMode, int duration, SpaceshipType player1Ship, SpaceshipType player2Ship, int difficulty, QObject *parent = 0)
     :QObject(parent),
